68b4e2e371320ef5e001dce5c5940650a44950cc
keemothy <kimchu@berkeley.edu>
2026-04-26 19:36:31 -0700

    finished login page styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,10 +3,15 @@
 import { useEffect } from "react";
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+// shadcn card components
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function LoginPage() {
   const router = useRouter();
 
+  // checking user session (if logged in then redirect to dashboard, o/w redirect to /login)
   useEffect(() => {
     const checkUser = async () => {
       const {
@@ -19,22 +24,40 @@ export default function LoginPage() {
     checkUser();
   }, [router]);
 
+  // handle login w/ google OAUTH
+
   const handleLogin = () => {
     window.location.href = "/api/google";
   };
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-4">
-      <div className="bg-white p-10 rounded-2xl shadow-xl flex flex-col items-center max-w-sm w-full">
-        <h1 className="text-3xl font-bold text-blue-600 mb-2">Macroflow</h1>
-
-        <button
-          onClick={handleLogin}
-          className="flex items-center justify-center w-full gap-3 px-6 py-3 border border-gray-300 rounded-lg shadow-sm hover:bg-gray-50 transition-all bg-white text-black font-medium"
-        >
-          Sign in with Google
-        </button>
-      </div>
+    <main
+      className="flex items-center justify-center min-h-screen p-4 bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?')`,
+      }}
+    >
+      <Card className="w-full max-w-sm shadow-lg border-none">
+        <CardHeader className="space-y-1 text-center">
+          <CardTitle className="text-3xl font-bold text-orange-600">
+            Macroflow
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <Button
+            variant="outline"
+            className="w-full py-6 text-base font-medium"
+            onClick={handleLogin}
+          >
+            <img
+              className="mr-2 h-5 w-5"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/3840px-Google_%22G%22_logo.svg.png"
+              alt="Google"
+            />
+            Sign in here with Google
+          </Button>
+        </CardContent>
+      </Card>
     </main>
   );
 }
