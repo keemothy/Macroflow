34c28e94c138b11b943cb07fbb431a6f839fca8b
keemothy <kimchu@berkeley.edu>
2026-04-28 21:33:51 -0700

    fixed localhost route for google

--- app/api/google/route.js
+++ app/api/google/route.js
@@ -3,7 +3,7 @@ import { supabase } from "@/lib/supabase";
 export async function GET(request) {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
-    options: { redirectTo: "http://localhost:3000/api/auth/callback" },
+    options: { redirectTo: `${window.location.origin}/auth/callback` },
   });
   if (error) {
     return new Response(error.message, { status: 500 });
