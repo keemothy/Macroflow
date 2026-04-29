67460a07239d7417b07fb77d3c4c36051bd98706
keemothy <kimchu@berkeley.edu>
2026-04-28 21:36:49 -0700

    added url for google

--- app/api/google/route.js
+++ app/api/google/route.js
@@ -1,12 +1,19 @@
-import { supabase } from "@/lib/supabase";
+import { supabase } from "@/utils/supabase/client";
 
 export async function GET(request) {
+  const { origin } = new URL(request.url);
+
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
-    options: { redirectTo: `${window.location.origin}/auth/callback` },
+    options: {
+      redirectTo: `${origin}/auth/callback`,
+    },
   });
+
   if (error) {
+    console.error("OAuth Error:", error.message);
     return new Response(error.message, { status: 500 });
   }
+
   return Response.redirect(data.url);
 }
